bbbc031c5063b68a994ca18d9efa6aff4926dcac
LeeSJ0109 <114146035+LeeSJ0109@users.noreply.github.com>
2024-07-24 14:59:02 +0900

    요세푸스 문제

---
+++ 1주차/BOJ_1158.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.StringTokenizer;
+
+public class BOJ_1158 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+
+        LinkedList<Integer> list = new LinkedList<>();
+        for (int i = 1; i <= N; i++) {
+            list.add(i);
+        }
+
+        sb.append("<");
+
+        while (!list.isEmpty()) {
+            for (int i = 0; i < K; i++) {
+                if (i == K - 1) {
+                    if (list.size() == 1) sb.append(list.remove());
+                    else sb.append(list.remove() + ", ");
+                } else {
+                    list.add(list.remove());
+                }
+            }
+        }
+        sb.append(">");
+        System.out.println(sb);
+    }
+}
