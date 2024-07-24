5ed8466fdb6984f300661f7b384d79769225544f
LeeSJ0109 <114146035+LeeSJ0109@users.noreply.github.com>
2024-07-24 14:51:54 +0900

    방 번호

---
+++ 1주차/BOJ_1475.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class BOJ_1475 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String N = sc.nextLine();
+
+        int [] num = new int[9];
+        for (int i = 0; i < N.length(); i++) {
+            int current_num = N.charAt(i) - '0';
+            if (current_num != 9) {
+                num[current_num]++;
+            } else {
+                num[6]++;
+            }
+        }
+        num[6] = (int) Math.round((double) num[6] / 2);
+
+        int max = 0;
+        for (int i = 0; i < 9; i++) {
+            if (num[i] > max) max = num[i];
+        }
+        System.out.println(max);
+        sc.close();
+    }
+}
