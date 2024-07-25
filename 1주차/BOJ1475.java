274645b199b5d09ef55cbbb659f2fd1a406d04a9
LeeSJ0109 <114146035+LeeSJ0109@users.noreply.github.com>
2024-07-26 07:59:06 +0900

    방번호

--- 1주차/BOJ1475.java
+++ 1주차/BOJ1475.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class BOJ_1475 {
+public class BOJ1475 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String N = sc.nextLine();
