aee1625e22ec9898a8efa7f625f7efcd30c9aea1
LeeSJ0109 <114146035+LeeSJ0109@users.noreply.github.com>
2024-07-26 07:59:24 +0900

    두 수의 합

--- 1주차/BOJ3273.java
+++ 1주차/BOJ3273.java
@@ -4,7 +4,7 @@ import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
-public class BOJ_3273 {
+public class BOJ3273 {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
