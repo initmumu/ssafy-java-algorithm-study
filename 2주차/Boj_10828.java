0b0cc35d98ecfc2d16598dd0d9216b9534142bde
beak1sin <89716846+beak1sin@users.noreply.github.com>
2024-07-25 17:48:18 +0900

    스택

---
+++ 2주차/Boj_10828.java
@@ -0,0 +1,54 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+public class Boj_10828 {
+
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		Stack<Integer> stack = new Stack<>();
+		
+		int N = Integer.parseInt(br.readLine());
+		for (int i = 0; i < N; i++) {
+			String s = br.readLine();
+			String a = s.split(" ")[0];
+			int b = 0;
+			if (a.equals("push")) b = Integer.parseInt(s.split(" ")[1]);
+			
+			
+			switch(a) {
+				case "push":
+					stack.push(b);
+					break;
+				case "pop":
+					if (stack.isEmpty()) {
+						System.out.println(-1);
+					} else {
+						System.out.println(stack.pop());
+					}
+					break;
+				case "size":
+					System.out.println(stack.size());
+					break;
+				case "empty":
+					if (stack.isEmpty()) {
+						System.out.println(1);
+					} else {
+						System.out.println(0);
+					}
+					break;
+				case "top":
+					if (stack.isEmpty()) {
+						System.out.println(-1);
+					} else {
+						System.out.println(stack.peek());
+					}
+					break;
+			}
+		}
+		
+	}
+
+}
