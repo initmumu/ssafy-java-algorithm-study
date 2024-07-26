dc4801e1a9366fa28f92bfabbdfcb51b244c68ef
beak1sin <89716846+beak1sin@users.noreply.github.com>
2024-07-26 15:35:42 +0900

    제로

---
+++ 2주차/Boj_10773.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+public class Boj_10773 {
+
+	public static void main(String[] args) throws IOException {
+		Stack<Integer> stack = new Stack<>();
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int K = Integer.parseInt(br.readLine());
+		int sum = 0;
+		for (int i = 0; i < K; i++) {
+			int n = Integer.parseInt(br.readLine());
+			if (n == 0) {
+				sum -= stack.pop();
+				continue;
+			}
+			stack.add(n);
+			sum += n;
+			
+		}
+		System.out.println(sum);
+	}
+
+}
