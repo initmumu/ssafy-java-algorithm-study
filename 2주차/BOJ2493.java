b01f2db0dea7edfb97d7c82c9fd08f6438fa2d25
이성준 <114146035+LeeSJ0109@users.noreply.github.com>
2024-07-30 08:01:38 +0900

    탑

--- 2주차/BOJ2493.java
+++ 2주차/BOJ2493.java
@@ -1,39 +1,40 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.Stack;
 import java.util.StringTokenizer;
 
 public class BOJ2493 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int N = Integer.parseInt(br.readLine());
 
         Stack<int[]> stack = new Stack<>();
-        int N = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
         StringTokenizer st = new StringTokenizer(br.readLine());
-        for (int i = 1; i <= N; i++) {
-            int height = Integer.parseInt(st.nextToken());
+        for (int i = 0; i < N; i++) {
+            int current_top_height = Integer.parseInt(st.nextToken());
 
-            if (stack.isEmpty()) {
-                System.out.print("0 ");
-                stack.push(new int[] {i, height});
-            } else {
-                while (true) {
-                    if (stack.isEmpty()) {
-                        System.out.print("0 ");
-                        stack.push(new int[]{i, height});
-                        break;
-                    }
-
-                    int[] top = stack.peek();
-
-                    if (top[1] > height) {
-                        System.out.print(top[0] + " ");
-                        stack.push(new int[]{i, height});
-                        break;
-                    } else stack.pop();
+            while (!stack.isEmpty()) {
+                if (stack.peek()[1] < current_top_height)
+                    stack.pop();
+                else {
+                    sb.append(stack.peek()[0])
+                            .append(' ');
+                    break;
                 }
             }
+
+            if (stack.empty())
+                sb.append("0 ");
+            stack.push(new int[] { i + 1, current_top_height });
         }
+
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
     }
 }
