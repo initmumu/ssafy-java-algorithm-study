e4e38d385c390f80dc027485467c554184a5c42f
beak1sin <89716846+beak1sin@users.noreply.github.com>
2024-07-30 15:43:43 +0900

    카드2

---
+++ 2주차/Boj_2164.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class Boj_2164 {
+
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		
+		Deque<Integer> deque = new ArrayDeque<Integer>();
+		
+		for (int i = 1; i <= N; i++) {
+			deque.addFirst(i);
+		}
+		
+		while(deque.size() > 1) {
+			deque.pollLast();
+			deque.addFirst(deque.pollLast());
+		}
+		
+		System.out.println(deque.pop());
+
+	}
+
+}
