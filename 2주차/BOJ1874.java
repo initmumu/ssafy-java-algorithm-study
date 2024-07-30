6a2ce4a769c4f3c7e99161b3384863cbc837e7c4
beak1sin <89716846+beak1sin@users.noreply.github.com>
2024-07-30 16:08:17 +0900

    스택 수열

--- 2주차/BOJ1874.java
+++ 2주차/BOJ1874.java
@@ -3,7 +3,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Stack;
 
-public class Boj_1874 {
+public class BOJ1874 {
 
 	public static void main(String[] args) throws IOException {
 		
