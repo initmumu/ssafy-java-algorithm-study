ef719d81859dedd72ac2c9935d1a2838e31047a7
beak1sin <89716846+beak1sin@users.noreply.github.com>
2024-07-30 16:04:03 +0900

    스택

--- 2주차/BOJ10773.java
+++ 2주차/BOJ10773.java
@@ -3,7 +3,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Stack;
 
-public class Boj_10773 {
+public class BOJ10773 {
 
 	public static void main(String[] args) throws IOException {
 		Stack<Integer> stack = new Stack<>();
