a0d3733c46f922df16cb2e2c171466bf03540c83
beak1sin <89716846+beak1sin@users.noreply.github.com>
2024-07-30 16:07:25 +0900

    오큰수

--- 2주차/BOJ17298.java
+++ 2주차/BOJ17298.java
@@ -4,7 +4,7 @@ import java.io.InputStreamReader;
 import java.util.Stack;
 import java.util.StringTokenizer;
 
-public class Boj_17298 {
+public class BOJ17298 {
 
 	public static void main(String[] args) throws IOException {
 		
