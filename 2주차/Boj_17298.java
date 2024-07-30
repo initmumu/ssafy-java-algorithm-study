def78e382ebeaf5f3f7f3dbad55868eec4103902
beak1sin <89716846+beak1sin@users.noreply.github.com>
2024-07-30 15:22:20 +0900

    오큰수

---
+++ 2주차/Boj_17298.java
@@ -0,0 +1,47 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+public class Boj_17298 {
+
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		
+		int[] arr = new int[N];
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		Stack<Integer> stack = new Stack<>();
+		StringBuilder sb = new StringBuilder();
+		for (int i = N-1; i >= 0; i--) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		for (int i = 0; i<N; i++) {
+			int n = arr[i];
+			StringBuilder sb2 = new StringBuilder();
+			
+			while(!stack.isEmpty()) {
+				if (stack.peek() > n) {
+					sb2.append(stack.peek()).reverse();
+					sb.append(" ").append(sb2);
+					break;
+				}
+				stack.pop();
+			}
+			
+			if (stack.isEmpty()) {
+				sb.append(" ").append("1-");
+			}
+			
+			stack.add(n);
+		}
+		
+		System.out.println(sb.reverse());
+
+	}
+
+}
