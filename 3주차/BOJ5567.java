8969fe441884a51d2f1ef4cb727e9895f13fe8e3
이성준 <114146035+LeeSJ0109@users.noreply.github.com>
2024-08-05 11:05:02 +0900

    결혼식

---
+++ 3주차/BOJ5567.java
@@ -0,0 +1,45 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int n = Integer.parseInt(br.readLine());
+        int m = Integer.parseInt(br.readLine());
+
+        boolean[] f1 = new boolean[n + 1];
+        boolean[] f2 = new boolean[n + 1];
+
+        Stack<int[]> stack = new Stack<>();
+
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            if (a == 1) f1[b] = true;
+            else if (b == 1) f1[a] = true;
+            else stack.push(new int[] {a, b});
+        }
+
+        while (!stack.isEmpty()) {
+            int a = stack.peek()[0];
+            int b = stack.pop()[1];
+
+            if (f1[a]) f2[b] = true;
+            else if (f1[b]) f2[a] = true;
+        }
+
+        int count = 0;
+        for (int i = 0; i < n+1; i++) {
+            if (f1[i] || f2[i]) count++;
+        }
+
+        System.out.println(count);
+    }
+}
