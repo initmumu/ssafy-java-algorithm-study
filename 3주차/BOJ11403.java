458e6296c288cdf319bd285d633ab122c1e6ab15
이성준 <114146035+LeeSJ0109@users.noreply.github.com>
2024-08-06 22:09:46 +0900

    경로 찾기

---
+++ 3주차/BOJ11403.java
@@ -0,0 +1,37 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System .in));
+		StringTokenizer st;
+		
+		int N = Integer.parseInt(br.readLine());
+		int[][] graph = new int[N][N];
+		
+		for (int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine());
+			for (int j = 0; j < N; j++) {
+				graph[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
+		
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < N; j++) {
+				for (int k = 0; k < N; k++) {
+					if (graph[j][i] == 1 && graph[i][k] == 1)
+						graph[j][k] = 1;
+				}
+			}
+		}
+		
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < N; j++) {
+				System.out.print(graph[i][j] + " ");
+			}
+			System.out.println();
+		}
+	}
+}
