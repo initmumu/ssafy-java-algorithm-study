db6f643939d6b4505f39a1783f6afd8924f80698
이성준 <114146035+LeeSJ0109@users.noreply.github.com>
2024-08-06 22:13:02 +0900

    결혼식

--- 3주차/BOJ5567.java
+++ 3주차/BOJ5567.java
@@ -4,7 +4,7 @@ import java.io.InputStreamReader;
 import java.util.Stack;
 import java.util.StringTokenizer;
 
-public class Main {
+public class BOJ5567 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
