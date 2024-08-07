1985d0296e27a47b2dd17d264053e0b024e21e88
beak1sin <89716846+beak1sin@users.noreply.github.com>
2024-08-07 22:48:34 +0900

    마법사 상어와 파이어볼

--- 3주차/BOJ20056.java
+++ 3주차/BOJ20056.java
@@ -1,18 +1,34 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.StringTokenizer;
 
-public class BOJ20056 {
+public class Main {
+	
+	public static class FireBall {
+		int r;
+		int c;
+		int m;
+		int s;
+		int d;
+		
+		public FireBall(int r, int c, int m, int s, int d) {
+			this.r = r;
+			this.c = c;
+			this.m = m;
+			this.s = s;
+			this.d = d;
+		}
+	}
 	
 	public static int N, M, K;
-	public static int graph[][];
-	public static int mass[][];
-	public static int direction[][];
-	public static int speed[][];
+	static List<FireBall>[][] graph;
+	static List<FireBall> fireballs = new ArrayList<>();
 	public static int [] dx = {-1, -1, 0, 1, 1, 1, 0, -1};
 	public static int [] dy = {0, 1, 1, 1, 0, -1, -1, -1};
+	
 	
 	public static int calc(int num) {
 		if (num % N == 0) {
@@ -22,106 +38,50 @@ public class BOJ20056 {
 	}
 	
 	public static void move() {
-//		임시 저장 배열들
-		int graph_temp[][] = new int[N+1][N+1];
-		int mass_temp[][] = new int[N+1][N+1];
-		int direction_temp[][] = new int[N+1][N+1];
-		int speed_temp[][] = new int[N+1][N+1];
-		
-		for (int i = 1; i <= N; i++) {
-			for (int j = 1; j <= N; j++) {
-				if (graph[i][j] == 4) {
-					System.out.println("호출");
-					int d = direction[i][j];
-					System.out.println(d);
-					int s = speed[i][j];
-					System.out.println(s);
-					while (d < 8) {
-						int nx = calc((N + i + dx[d] * (s % N)) % N); 
-						int ny = calc((N + j + dy[d] * (s % N)) % N); 
-						System.out.println("nx: " + nx + ", ny: " + ny);
-						graph_temp[nx][ny] += graph[i][j] / 4;
-						mass_temp[nx][ny] += mass[i][j];
-						direction_temp[nx][ny] += direction[i][j];
-						speed_temp[nx][ny] += speed[i][j];
-						d += 2;
-					}
-//					graph[i][j] = 0;
-//					mass[i][j] = 0;
-//					direction[i][j] = 0;
-//					speed[i][j] = 0;
-				}
-			}
-		}
-		
-		for (int i = 1; i <= N; i++) {
-			for (int j = 1; j <= N; j++) {
-//				if (graph[i][j] == 4) {
-//					System.out.println("호출");
-//					int d = direction[i][j];
-//					int s = speed[i][j];
-//					for (int k = 0; k < 4; k +=2) {
-//						d += k;
-//						int nx = calc((N + i + dx[d] * (s % N)) % N); 
-//						int ny = calc((N + j + dy[d] * (s % N)) % N); 					
-//						graph_temp[nx][ny] += graph[i][j];
-//						mass_temp[nx][ny] += mass[i][j];
-//						direction_temp[nx][ny] += direction[i][j];
-//						speed_temp[nx][ny] += speed[i][j];
-//					}
-//					graph[i][j] = 0;
-//					continue;
-//				}
-				if (graph[i][j] > 0) {
-					int d = direction[i][j];
-					int s = speed[i][j];
-					int nx = calc((N + i + dx[d] * (s % N)) % N); 
-					int ny = calc((N + j + dy[d] * (s % N)) % N); 					
-					graph_temp[nx][ny] += graph[i][j];
-					mass_temp[nx][ny] += mass[i][j];
-					direction_temp[nx][ny] += direction[i][j];
-					speed_temp[nx][ny] += speed[i][j];
-				}
-			}
-		}
-		
-		
-//		임시 저장한 배열들 복구
-		for (int i = 1; i <= N; i++) {
-			for (int j = 1; j <= N; j++) {				
-				graph[i][j] = graph_temp[i][j];
-				mass[i][j] = mass_temp[i][j];
-				direction[i][j] = direction_temp[i][j];
-				speed[i][j] = speed_temp[i][j];
-			}
+		for (FireBall ball: fireballs) {
+			int nx = calc((N + ball.r+ dx[ball.d] * (ball.s % N)) % N); 
+			int ny = calc((N + ball.c + dy[ball.d] * (ball.s % N)) % N); 
+			ball.r = nx;
+			ball.c = ny;
+			graph[nx][ny].add(ball);
 		}
 	}
 	
 	public static void fireballTwice() {
 		for (int i = 1; i <= N; i++) {
 			for (int j = 1; j <= N; j++) {
-				if (graph[i][j] >= 2) {
-//					질량은 합쳐진 파이어볼 질량의 합 / 5
-					mass[i][j] = (mass[i][j] / 5);
-//					속력은 합쳐진 파이어볼 속력의 합 / 합쳐진 파이어볼의 개수
-					speed[i][j] = speed[i][j] / graph[i][j];
-					
-					if (direction[i][j] % graph[i][j] == 0) {
-						// 모두 짝수 또는 모두 홀수
-						direction[i][j] = 0;
-					} else {
-						direction[i][j] = 1;
-					}
-					graph[i][j] = 4;
+				if (graph[i][j].size() <= 1) {
+					graph[i][j].clear();
+					continue;
 				}
-			}
-		}
-	}
-	public static void massZero() {
-		for (int i = 1; i <= N; i++) {
-			for (int j = 1; j <= N; j++) {
-				if (mass[i][j] == 0) {
-					graph[i][j] = 0;
+				int massSum = 0;
+				int speedSum = 0;
+				boolean even = graph[i][j].get(0).d % 2 == 0 ? true : false;
+				boolean odd = graph[i][j].get(0).d % 2 == 1 ? true : false;
+				for (FireBall f : graph[i][j]) {
+					massSum += f.m;
+					speedSum += f.s;
+					even = even & f.d % 2 == 0 ? true : false;
+					odd = odd & f.d % 2 == 1 ? true : false;
+					fireballs.remove(f);
+				}
+				int nMass = massSum / 5;
+				int size = graph[i][j].size();
+				graph[i][j].clear();
+				int nSpeed = speedSum / size;
+
+				if (nMass == 0) {
+					continue;
+				}
+
+				if (even || odd) { // 0 2 4 6
+					for (int k = 0; k < 8; k += 2) {
+						fireballs.add(new FireBall(i, j, nMass, nSpeed, k));
+					}
+				} else {
+					for (int k = 1; k < 8; k += 2) {
+						fireballs.add(new FireBall(i, j, nMass, nSpeed, k));
+					}
 				}
 			}
 		}
@@ -135,42 +95,33 @@ public class BOJ20056 {
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
 		K = Integer.parseInt(st.nextToken());
-		graph = new int [N+1][N+1];
-		mass = new int [N+1][N+1];
-		direction = new int [N+1][N+1];
-		speed = new int [N+1][N+1];
+		graph = new ArrayList[N+1][N+1];
+		for (int i = 1; i <= N; i++) {
+			for (int j = 1; j <= N; j++) {
+				graph[i][j] = new ArrayList<>();
+			}
+		}
 		for (int i = 0; i < M; i++) {
-//			처음 모든 파이어볼들의 위치는 다 다른지?? 일단 스킵
 			st = new StringTokenizer(br.readLine());
 			int r = Integer.parseInt(st.nextToken());
 			int c = Integer.parseInt(st.nextToken());
 			int m = Integer.parseInt(st.nextToken());
 			int s = Integer.parseInt(st.nextToken());
 			int d = Integer.parseInt(st.nextToken());
-			graph[r][c] += 1;
-			mass[r][c] = m;
-			direction[r][c] = d;
-			speed[r][c] = s;
+			fireballs.add(new FireBall(r, c, m, s, d));
 		}
-		System.out.println(Arrays.deepToString(graph));
+		
 		for (int i = 0; i < K; i++) {
 //			d 방향, s 속력 만큼 이동
 			move();
-			System.out.println(Arrays.deepToString(graph));
-//			2개 이상 파이어볼 작업
+			
+//			2개 이상 파이어볼 작업 , 질량 0인 파이어볼 삭제
 			fireballTwice();
 			
-//			질량 0인 파이어볼 삭제
-			massZero();
-			System.out.println(Arrays.deepToString(graph));
 		}
 		int result = 0;
-		for (int i = 1; i <= N; i++) {
-			for (int j = 1; j <= N; j++) {
-				if (graph[i][j] > 0) {
-					result += mass[i][j];
-				}
-			}
+		for (FireBall f: fireballs) {
+			result += f.m;
 		}
 		System.out.println(result);
 	}
