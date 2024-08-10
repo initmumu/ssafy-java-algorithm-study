4513a8580216065f3cbcd298ae7738d5c9f0104b
이성준 <114146035+LeeSJ0109@users.noreply.github.com>
2024-08-11 03:04:33 +0900

    N과 M (2)

--- 4주차/BOJ15650.java
+++ 4주차/BOJ15650.java
@@ -1,40 +1,38 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+    import java.io.BufferedReader;
+    import java.io.IOException;
+    import java.io.InputStreamReader;
+    import java.util.StringTokenizer;
 
-public class BOJ15650 {
+    public class BOJ15650 {
 
-    static int N, M;
-    static int[] combination;
-    static boolean[] visited;
+        static int N, M;
+        static int[] combination;
 
-    static StringBuilder sb = new StringBuilder();
+        static StringBuilder sb = new StringBuilder();
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
+        public static void main(String[] args) throws IOException {
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            StringTokenizer st = new StringTokenizer(br.readLine());
 
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
+            N = Integer.parseInt(st.nextToken());
+            M = Integer.parseInt(st.nextToken());
 
-        combination = new int[M];
-        visited = new boolean[N + 1];
+            combination = new int[M];
 
-        System.out.println(combinations(0, 1));
-    }
-
-    static StringBuilder combinations (int depth, int cur) {
-        if (depth == M) {
-            for (int p: combination) sb.append(p + " ");
-            sb.append("\n");
+            System.out.println(combinations(0, 1));
         }
-        else {
-            for (int i = cur; i <= N; i++) {
-                combination[depth] = i;
-                combinations(depth + 1, i + 1);
+
+        static StringBuilder combinations (int depth, int cur) {
+            if (depth == M) {
+                for (int p: combination) sb.append(p + " ");
+                sb.append("\n");
             }
+            else {
+                for (int i = cur; i <= N; i++) {
+                    combination[depth] = i;
+                    combinations(depth + 1, i + 1);
+                }
+            }
+            return sb;
         }
-        return sb;
     }
-}
