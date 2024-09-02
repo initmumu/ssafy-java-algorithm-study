4929ace250896a294d7368c615b67a8b5fd5bbed
이성준 <114146035+LeeSJ0109@users.noreply.github.com>
2024-09-02 22:17:15 +0900

    암호문

--- 7주차/SWEA1230.java
+++ 7주차/SWEA1230.java
@@ -4,7 +4,7 @@ import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.StringTokenizer;
 
-public class SWEA_1230 {
+public class SWEA1230 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
