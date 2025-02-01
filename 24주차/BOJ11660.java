945f082f7d756cdd77a533e69215cfc351b60267
beak1sin <89716846+beak1sin@users.noreply.github.com>
2025-02-01 17:49:33 +0900

    Create BOJ11660.java

---
+++ 24주차/BOJ11660.java
@@ -0,0 +1,38 @@
+import java.io.*;
+import java.util.*;
+
+public class BOJ11660 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        
+        int[][] arr = new int[N + 1][N + 1];
+        int[][] prefixSum = new int[N + 1][N + 1];
+        
+        for (int i = 1; i <= N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 1; j <= N; j++) {
+                arr[i][j] = Integer.parseInt(st.nextToken());
+                prefixSum[i][j] = arr[i][j] + prefixSum[i - 1][j] + prefixSum[i][j - 1] - prefixSum[i - 1][j - 1];
+            }
+        }
+        
+        StringBuilder sb = new StringBuilder();
+        
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int x1 = Integer.parseInt(st.nextToken());
+            int y1 = Integer.parseInt(st.nextToken());
+            int x2 = Integer.parseInt(st.nextToken());
+            int y2 = Integer.parseInt(st.nextToken());
+            
+            int result = prefixSum[x2][y2] - prefixSum[x1 - 1][y2] - prefixSum[x2][y1 - 1] + prefixSum[x1 - 1][y1 - 1];
+            sb.append(result).append("\n");
+        }
+        
+        System.out.print(sb.toString());
+    }
+}
